01356bb1c72397a008a97fb8856d559f398e8e55
thomijasir <thomijasir@gmail.com>
2026-08-29 11:11:28 +0800

    feat(bootstrap): define contracts

---
+++ app/src/modules/bootstrap/model.rs
@@ -0,0 +1,21 @@
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+#[derive(Serialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct BootstrapStatus {
+    pub state: &'static str,
+}
+#[derive(Deserialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct BootstrapAdminRequest {
+    pub setup_token: String,
+    pub email: String,
+    pub password: String,
+}
+#[derive(Serialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct BootstrapAdminResponse {
+    pub admin_id: String,
+    pub email: String,
+    pub csrf_token: String,
+}
