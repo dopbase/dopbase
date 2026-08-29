6680623730943d50300fe7b16119184b27554ff0
thomijasir <thomijasir@gmail.com>
2026-08-29 11:11:30 +0800

    refactor(cli): assemble the CLI module

---
+++ app/src/cli/mod.rs
@@ -0,0 +1,11 @@
+mod args;
+mod client;
+mod commands;
+mod dotenv;
+mod local_config;
+
+pub use args::Cli;
+
+pub async fn execute(cli: Cli) -> anyhow::Result<i32> {
+    commands::execute(cli).await
+}
