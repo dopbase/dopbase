923f67468bb6b9c1b14027a6041defaa2528fc5a
thomijasir <thomijasir@gmail.com>
2026-08-29 11:11:27 +0800

    feat(core): add configuration constants

---
+++ app/src/constants/config.rs
@@ -0,0 +1,15 @@
+pub const DEFAULT_BIND_ADDRESS: &str = "127.0.0.1:8376";
+pub const DEFAULT_PUBLIC_URL: &str = "http://localhost:8376";
+pub const DATA_DIRECTORY_NAME: &str = ".dopbase";
+pub const DATABASE_FILENAME: &str = "dopbase.db";
+pub const CLIENT_CONFIG_FILENAME: &str = "config.toml";
+pub const MASTER_KEY_FILENAME: &str = "master.key";
+pub const SERVER_CONFIG_FILENAME: &str = "server.toml";
+
+pub const ENV_BIND_ADDRESS: &str = "DOPBASE_BIND_ADDRESS";
+pub const ENV_DATA_DIR: &str = "DOPBASE_DATA_DIR";
+pub const ENV_DATABASE_URL: &str = "DOPBASE_DATABASE_URL";
+pub const ENV_MASTER_KEY_PATH: &str = "DOPBASE_MASTER_KEY_PATH";
+pub const ENV_PUBLIC_URL: &str = "DOPBASE_PUBLIC_URL";
+pub const ENV_SERVER_URL: &str = "DOPBASE_URL";
+pub const ENV_SHUTDOWN_GRACE_SECONDS: &str = "DOPBASE_SHUTDOWN_GRACE_SECONDS";
