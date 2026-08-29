fe2d02dbd2b7bbd6d30f07455720b6c2fadee7c4
thomijasir <thomijasir@gmail.com>
2026-08-29 11:11:28 +0800

    feat(server): define application state

---
+++ app/src/state.rs
@@ -0,0 +1,22 @@
+use std::sync::Arc;
+
+use tokio::sync::RwLock;
+
+use crate::{
+    config::ServerConfig,
+    services::{cache::RateLimiter, crypto::CryptoService, db::DbClient},
+};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub config: Arc<ServerConfig>,
+    pub db: DbClient,
+    pub crypto: CryptoService,
+    pub setup: Arc<RwLock<SetupState>>,
+    pub rate_limiter: RateLimiter,
+}
+
+#[derive(Default)]
+pub struct SetupState {
+    pub token: Option<String>,
+}
