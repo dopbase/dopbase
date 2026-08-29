bcf3755731b8ddd176dd2b471c32d8a6c05955cf
thomijasir <thomijasir@gmail.com>
2026-08-29 11:11:28 +0800

    feat(health): add HTTP handlers

---
+++ app/src/modules/health/controller.rs
@@ -0,0 +1,6 @@
+use super::{error::HealthError, model::HealthResponse, service};
+use crate::http::{ErrorBody, HttpResponse, HttpResponseFormat};
+#[utoipa::path(get, path="/api/v1/health", tag="health", responses((status=200, body=inline(HttpResponseFormat<HealthResponse>)), (status=500, body=ErrorBody)))]
+pub async fn health() -> Result<HttpResponse<HealthResponse>, HealthError> {
+    Ok(HttpResponse::ok(service::health(), "HEALTH_OK"))
+}
