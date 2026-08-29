54ccdc3ae76082c1f56de5a732819bd09ce23cf3
thomijasir <thomijasir@gmail.com>
2026-08-29 11:11:27 +0800

    feat(core): add token prefixes

---
+++ app/src/constants/tokens.rs
@@ -0,0 +1,8 @@
+pub const ADMIN_ID_PREFIX: &str = "usr_";
+pub const ADMIN_SESSION_PREFIX: &str = "dbc_";
+pub const CSRF_TOKEN_PREFIX: &str = "csrf_";
+pub const ENVIRONMENT_ID_PREFIX: &str = "env_";
+pub const PROJECT_ID_PREFIX: &str = "prj_";
+pub const RUNNER_TOKEN_ID_PREFIX: &str = "tok_";
+pub const RUNNER_TOKEN_PREFIX: &str = "dbs_";
+pub const SESSION_ID_PREFIX: &str = "ses_";
