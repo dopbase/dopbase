80e7d341f872fc8f219db0942ff4bdc298476f63
thomijasir <thomijasir@gmail.com>
2026-08-29 11:11:30 +0800

    feat(instance): assemble routes

---
+++ app/src/modules/instance/mod.rs
@@ -0,0 +1,11 @@
+use crate::state::AppState;
+use axum::{Router, routing::get};
+pub mod controller;
+pub mod doc;
+pub mod error;
+pub mod model;
+mod repository;
+pub mod service;
+pub fn routes() -> Router<AppState> {
+    Router::new().route("/api/v1/instance", get(controller::status))
+}
