b77bd6b1a35f203352506020e6a98cfc134c8238
thomijasir <thomijasir@gmail.com>
2026-08-29 11:11:28 +0800

    feat(health): add OpenAPI schema

---
+++ app/src/modules/health/doc.rs
@@ -0,0 +1,9 @@
+use super::{controller, model::HealthResponse};
+use crate::http::{ErrorBody, HttpResponseFormat};
+use utoipa::OpenApi;
+#[derive(OpenApi)]
+#[openapi(paths(controller::health), components(schemas(HealthResponse, ErrorBody, HttpResponseFormat<HealthResponse>)), tags((name="health")))]
+struct HealthApi;
+pub fn build() -> utoipa::openapi::OpenApi {
+    HealthApi::openapi()
+}
