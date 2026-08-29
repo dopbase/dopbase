8356bd91fd827b202572ae413b1a7fad0118bd06
thomijasir <thomijasir@gmail.com>
2026-08-29 11:11:28 +0800

    feat(health): define contracts

---
+++ app/src/modules/health/model.rs
@@ -0,0 +1,10 @@
+use serde::Serialize;
+use utoipa::ToSchema;
+#[derive(Serialize, ToSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct HealthResponse {
+    pub product: &'static str,
+    pub version: &'static str,
+    pub api_version: &'static str,
+    pub status: &'static str,
+}
